a2660a2e4a0bdeb87e66a86d7a1aba411dea4d4a
kelvinchrisman <chrismankelvin@gmail.com>
2023-01-28 14:42:46 +0000

    teleprogsignsuccesfull

--- progsignin.php
+++ progsignin.php
@@ -3,31 +3,55 @@ require 'header.php'
 ?>
 <div class="main">
     <div class="container">
-    <div class="holder">
-        <form action="./scripts/signup.php" method="post">
-            <p>SIGN IN</p>
-            <fieldset>
-                <label for="name">Enter Your Name</label> <br>
-                <label for="email">Enter Your E-Mail</label><br>
-                <label for="county">Enter Your Country</label><br>
-                <label for="grpname"> Enter Group's Name</label><br>
-                <label for="grplink">Enter Group Link</label>
-            </fieldset>
-            <input type="text" name="name" placeholder="Full Name" autocomplete="off"> <br>
-            <input type="text" name="email" placeholder="E-Mail Address" autocomplete="off"> <br>
-            <input type="text" name="country" placeholder="Your Country" autocomplete="off"><br>
-            <input type="text" name="grpname" placeholder="Group's Name" autocomplete="off"><br>
-            <input type="text" name="grplink" placeholder="Group's Link" autocomplete="off"><br>
-            <button name="submit" type="submit">
-                submit
-            </button>
-        </form>
-    </div>
+        <div class="holder">
+            <form action="./inc/postman.php" method="POST">
+                <p>SIGN IN</p>
+                <fieldset>
+                    <label for="name">Enter Your Name</label> <br>
+                    <label for="email">Enter Your E-Mail</label><br>
+                    <label for="county">Enter Your Country</label><br>
+                    <label for="grpname"> Enter Group's Name</label><br>
+                    <label for="grplink">Enter Group Link</label>
+                </fieldset>
+                <input type="text" name="name" placeholder="Full Name" autocomplete="off"> <br>
+                <input type="text" name="email" placeholder="E-Mail Address" autocomplete="off"> <br>
+                <input type="text" name="country" placeholder="Your Country" autocomplete="off"><br>
+                <input type="text" name="grpname" placeholder="Group's Name" autocomplete="off"><br>
+                <input type="text" name="grplink" placeholder="Group's Link" autocomplete="off"><br>
+                <button name="submit" type="submit">
+                    submit
+                </button>
+                <?php
+                if (isset($_GET['error'])) {
+                    if ($_GET['error'] == "emptyfields") {
+                        echo '<p class="error">Fill out the Form</p>';
+                    } elseif ($_GET['error'] == "noname") {
+                        echo '<p class="error">Enter your name</p>';
+                    } elseif ($_GET['error'] == "noemail") {
+                        echo '<p class="error">Enter your E-mail</p>';
+                    } elseif ($_GET['error'] == "nocountry") {
+                        echo '<p class="error">enter your location</p>';
+                    } elseif ($_GET['error'] == "nogrpname") {
+                        echo '<p class="error">Enter your Telegram group name</p>';
+                    } elseif ($_GET['error'] == "nogrplink") {
+                        echo '<p class="error">Enter the link to your group</p>';
+                    } elseif ($_GET['error'] == "nogrplink") {
+                        echo '<p class="error">Enter the link to your group</p>';
+                    } elseif ($_GET['error'] == "invalidemail") {
+                        echo '<p class="error">Invalid e-mail</p>';
+                    }elseif($_GET['error'] == "invalidename"){
+                        echo '<p class="error">Invalid name/p>';
+                }
+                }
+
+                ?>
+            </form>
+        </div>
 
     </div>
 
 </div>
 <?php
-require'footer.php'
+require 'footer.php'
 
 ?>
