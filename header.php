203c195b151db642bd65444c4f477b095e76f574
kelvinchrisman <chrismankelvin@gmail.com>
2023-02-11 12:18:05 +0000

    rec_cont

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./inc/style.css">
+    <link rel="stylesheet" href="./inc/style.css?v=1">
     <script src="./inc/main.js"></script>
     <title></title>
 </head>
@@ -28,7 +28,7 @@
         <li><a href="#">Sucess Rate Calculator (SRC)</a></li>
         <li><a href="#">required documents</a></li>
         <li><a href="#">Mr. Richard Agyei</a></li>
-        <li><a href="#">contact</a></li>
+        <li><a href="./social.php">contact</a></li>
    
     </ul>
 
--- services.php
+++ services.php
@@ -3,27 +3,39 @@ require 'header.php'
 ?>
 <div class="main">
     <div class="container">
-    <div class="index_top_cont">
+        <div class="index_top_cont">
             <h1>Book An Apointment</h1>
             <p>About 80% of the leaders in Africa, both in business and politics, had at least 20% of their Education Abroad.</p>
         </div>
         <div class="apointment">
-            <br>
-            <p>click on the link below to book an appointment with REC <br>
-                we will create a special time for you to listen to you,
-                Please understand that by filling the form you will join a quew <br>
-                keep calm as we process your Apointment<br>
-                Click Here To Get Started.
-            </p>
-        
-        <div class="onebut">
+            <img src="./inc/files/apointment.jpg" alt="">
+            <div class="apoint">
+
+                <h3>
+                    Book <br> Apointment
+
+                </h3>
+
+                <p>click on the button below to book an appointment with REC 
+                    we will create a special time for you to listen to you,
+                    Please understand that by filling the form 
+                    you will join a quew keep calm as we process your Apointment
+                    
+                </p>
+                <div class="onebut">
             <button class="onebutton">
                 <a href="./progsignin.php">Get Started</a>
             </button></div>
+            </div>
+
+         
 
         </div>
 
     </div>
+    <div class="index_down_cont">
+            Image Cards
+        </div>
 
 
 
