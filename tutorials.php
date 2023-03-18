6f5bd80b0c6f67cf4e76b0ddaac0cc4fc5ae9986
kelvinchrisman <chrismankelvin@gmail.com>
2023-03-18 08:10:27 +0000

    fixing

--- tutorials.php
+++ tutorials.php
@@ -14,22 +14,51 @@ require 'header.php'
 
             <div class="service1">
                 <h2>Study Abroad</h2>
+                <p>
+                    Welcome to <strong> Richard Educational Consult!</strong>  We are a team of professionals who are passionate about helping students broaden their horizons through studying abroad. We understand that studying in a foreign country can be a life-changing experience, and we are here to guide you every step of the way.
+                    Our study abroad services cover a wide range of programs, including language courses, undergraduate and postgraduate programs, summer schools, internships, and exchange programs. We work with top universities and institutions around the world to provide you with the best opportunities to achieve your academic and career goals.
+                    Our team of experts provides personalized guidance to help you select the program that best suits your needs and interests. We assist you with the application process, including preparing your application materials, submitting your application, and securing your admission. We also provide visa support and assistance with travel arrangements, such as flights and accommodation.
+                    At Richard Educational Consult, we believe that studying abroad is not only a great way to gain academic and professional experience, but also to broaden your cultural horizons and develop lifelong skills. Our mission is to help you make the most of your study abroad experience by providing you with the best possible support and guidance.
+                    <strong><a href="" class="clickhere">Contact </a></strong> us today to learn more about our study abroad services and how we can help you achieve your academic and career aspirations through studying abroad.
+                </p>
+
+
+
+            </div>
+
+            <div class="service4">
+                <h2>scholarship</h2>
+                <p>
+                    Welcome to Richard Educational Consult! We are committed to helping students achieve their academic goals by providing access to a range of scholarship opportunities. Our team of experts works tirelessly to identify scholarships from various sources, including foundations, corporations, government agencies, and universities.
+                    Our scholarship offerings cover a wide range of academic disciplines and levels, from undergraduate to postgraduate studies. Whether you are a high school student planning to pursue a degree in engineering or a graduate student looking for funding to support your research project, we have scholarships that may be right for you.
+                    Our scholarship database is regularly updated with the latest opportunities, so be sure to check back frequently for new listings. In addition, we provide personalized guidance to help you navigate the scholarship application process, from identifying suitable scholarships to preparing strong applications that stand out to selection committees.
+                    At Richard Educational Consult, we believe that access to education should not be limited by financial constraints. That's why we are dedicated to connecting students with scholarship opportunities that can help them achieve their academic and career aspirations. Contact us today to learn more about our scholarship offerings and how we can support your academic journey.
+
+                </p>
 
             </div>
             <div class="service2">
                 <h2>Immigration</h2>
+                <p>
+
+                    Richard Educational Consult offers immigration services to individuals seeking to move to a new country for various purposes, such as work, study, or permanent residency. Our team of experts provides personalized guidance and support to help you navigate the complex immigration process. We offer assistance with visa applications, documentation, and legal requirements to ensure a smooth and successful immigration process. Contact us today to learn more about our immigration services and how we can help you achieve your immigration goals.
+
+                </p>
 
             </div>
             <div class="service3">
                 <h2>Visa Issues</h2>
+                <p>
+                    Richard Educational Consult offers personalized guidance and support to individuals who require assistance with visa issues. Our team of experts provides guidance on the visa application process, including the required documentation, eligibility criteria, and legal requirements. We offer visa guidance services for various purposes, such as study, work, and travel. Our goal is to help you navigate the visa process successfully and ensure that you have the necessary documentation to obtain your visa. Contact us today to learn more about our visa guidance services and how we can help you.
 
-            </div>
-            <div class="service4">
-                <h2>scholarship</h2>
+                </p>
 
             </div>
             <div class="service5">
                 <h2>Other service</h2>
+                <p>
+
+                </p>
 
             </div>
 
@@ -54,10 +83,19 @@ require 'header.php'
                 <li><a href="">mississipi State Uni</a></li>
                 <li><a href="">minnesota State Uni</a></li>
                 <li><a href="">arkanas state Uni</a></li>
-                <li><a href="">Kansas state university</a></li>
-                <li><a href="">liberty university</a></li>
-                <li><a href="">sonoma state university</a></li>
-                <li><a href="">san francisco Uni</a></li>
+
+
+                <li><a href="">University of Oxford, UK</a></li>
+                <li><a href="">Harvard University, USA</a></li>
+                <li><a href="">University of Cambridge, UK</a></li>
+                <li><a href="">Stanford University, USA</a></li>
+                <li><a href="">Massachusetts Institute of Technology (MIT), USA</a></li>
+                <li><a href="">ETH Zurich – Swiss Federal Institute of Technology Zurich, Switzerland</a></li>
+                <li><a href="">University of Toronto, Canada</a></li>
+                <li><a href="">University of Melbourne, Australia</a></li>
+                <li><a href="">National University of Singapore (NUS), Singapore</a></li>
+                <li><a href="">University of Tokyo, Japan</a></li>
+                <!-- <li><a href="">san francisco Uni</a></li>
                 <li><a href="">mississipi State Uni</a></li>
                 <li><a href="">minnesota State Uni</a></li>
                 <li><a href="">arkanas state Uni</a></li>
@@ -68,14 +106,7 @@ require 'header.php'
                 <li><a href="">mississipi State Uni</a></li>
                 <li><a href="">minnesota State Uni</a></li>
                 <li><a href="">arkanas state Uni</a></li>
-                <li><a href="">Kansas state university</a></li>
-                <li><a href="">liberty university</a></li>
-                <li><a href="">sonoma state university</a></li>
-                <li><a href="">san francisco Uni</a></li>
-                <li><a href="">mississipi State Uni</a></li>
-                <li><a href="">minnesota State Uni</a></li>
-                <li><a href="">arkanas state Uni</a></li>
-                <li><a href=""></a></li>
+                <li><a href=""></a></li> -->
             </ul>
 
         </div>
